3048ebc567085cd2c90a0bfc059f2ae8397e7e86
AlexandrShipovsky <shipovsky.alexandr@yandex.ru>
2019-07-30 11:36:41 +0300

    Добавлены настройки тактирования

--- main.c
+++ main.c
@@ -14,6 +14,7 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f10x.h"
+#include "stm32f10x_rcc.h"
 #include "stm32f10x_gpio.h"
 #include "UART/UART.h"
 #include "stm32f10x_dma.h"
@@ -37,7 +38,8 @@
 char buf[4]; // Кольцевой буфер
 /* Private function prototypes -----------------------------------------------*/
 void Delay_ms(uint32_t ms);
-void DMA_Ini(void);
+void DMA_ini(void);
+ErrorStatus RCC_ini(void);
 /* Private functions ---------------------------------------------------------*/
 // Программная задержка
 void Delay_ms(uint32_t ms)
@@ -45,6 +47,42 @@ void Delay_ms(uint32_t ms)
   for (uint32_t i = 0; i < ms; i++)
   {
   }
+}
+// Настройки тактирования
+ErrorStatus RCC_ini(void)
+{
+  RCC_DeInit();              //Сброс настроек
+  RCC_HSEConfig(RCC_HSE_ON); //Включаем внешний кварцевый резонатор
+  ErrorStatus HSEStartUpStatus;
+  HSEStartUpStatus = RCC_WaitForHSEStartUp(); /* Ждем пока HSE будет готов */
+  if (HSEStartUpStatus == ERROR)
+  {
+    return HSEStartUpStatus;
+  }
+  /* HCLK = SYSCLK */ /* Смотри на схеме AHB Prescaler. Частота не делится (RCC_SYSCLK_Div1) */
+  RCC_HCLKConfig(RCC_SYSCLK_Div1);
+  /* PCLK2 = HCLK */ /* Смотри на схеме APB2 Prescaler. Частота не делится (RCC_HCLK_Div1)  */
+  RCC_PCLK2Config(RCC_HCLK_Div1);
+  /* PCLK1 = HCLK/2 */ /* Смотри на схеме APB1 Prescaler. Частота делится на 2 (RCC_HCLK_Div2)
+	потому что на выходе APB1 должно быть не более 36МГц (смотри схему тактирования) */
+  RCC_PCLK1Config(RCC_HCLK_Div2);
+  /* PLLCLK = 8MHz * 9 = 72 MHz */
+  /* Указываем PLL от куда брать частоту (RCC_PLLSource_HSE_Div1) и на сколько ее умножать (RCC_PLLMul_9) */
+  /* PLL может брать частоту с кварца как есть (RCC_PLLSource_HSE_Div1) или поделенную на 2 (RCC_PLLSource_HSE_Div2). Смотри схему */
+  RCC_PLLConfig(RCC_PLLSource_HSE_Div1, RCC_PLLMul_9);
+  /* Включаем PLL */
+  RCC_PLLCmd(ENABLE);
+  /* Ждем пока PLL будет готов */
+  while (RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET)
+  {
+  };
+  /* Переключаем системное тактирование на PLL */
+  RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);
+  /* Ждем пока переключиться */
+  while (RCC_GetSYSCLKSource() != 0x08)
+  {
+  };
+  return HSEStartUpStatus;
 }
 // Настройка DMA
 void DMA_ini(void)
@@ -75,7 +113,8 @@ void DMA_ini(void)
   */
 int main(void)
 {
-
+  ErrorStatus RCCStatus;
+  RCCStatus = RCC_ini();
   UART_Init();
   DMA_ini();
   /*!< At this stage the microcontroller setting
@@ -88,8 +127,14 @@ int main(void)
   PIN_INIT.GPIO_Speed = GPIO_Speed_10MHz;
   PIN_INIT.GPIO_Mode = GPIO_Mode_Out_OD;
   GPIO_Init(GPIOC, &PIN_INIT);
-
-  //Send_UART_Str(USART1, "I'm ready!\n\r");
+  if (RCCStatus)  // Проверка настроек тактирования
+  {
+    Send_UART_Str(USART1, "I'm ready!\n\rRCC SUCCESS\n\r");
+  }else
+  {
+    Send_UART_Str(USART1, "I'm ready!\n\rRCC ERROR\n\r");
+  }
+  
   uint8_t i = 0;
   while (1)
   {
